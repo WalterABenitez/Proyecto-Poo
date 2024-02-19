d01531e3119b5fe942e85680171af176c4f1232c
Hernan Rubio <hernan.andres.rubio@gmail.com>
2024-02-19 09:09:17 -0300

    DISEÑO: Se agregaron tooltips a todos los botones de las tablas de las paginas. HR

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -8,6 +8,9 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    console.log(tooltipList)
     // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
